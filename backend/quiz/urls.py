53f0699702470af05a1892a6b2911a0cf4ce66db
duplxey <nick.tomazic@gmail.com>
2020-02-25 13:04:21 +0100

    Fixed name problem.

--- backend/quiz/urls.py
+++ backend/quiz/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('statistics/<int:quiz_id>/', views.QuizStats.as_view()),
     path('result/<int:quiz_id>/<int:user_id>/', views.QuizResult.as_view()),
     path('leaderboard/<int:quiz_id>/', views.QuizLeaderboard.as_view()),
-    path('generate/<int:resource_id>/', views.QuizGenerate.as_view()),
+    path('generate/<int:material_id>/', views.QuizGenerate.as_view()),
 ]
